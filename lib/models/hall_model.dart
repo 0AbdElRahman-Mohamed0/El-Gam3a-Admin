595f9fa15732d8e0d3d13919d03b1e6f24e5876a
kira <sci.bedoMohamed2018@alexu.edu.eg>
2021-03-13 10:06:31 -0500

    when adding a new hall add times with it

--- lib/models/hall_model.dart
+++ lib/models/hall_model.dart
@@ -4,7 +4,14 @@ class HallModel {
   int id;
   int capacity;
   // String location;
-  List<String> choosedTimes;
+  List<String> choosedTimes = [
+    '8:00 AM - 10:00 AM',
+    '10:00 AM - 12:00 PM',
+    '12:00 PM - 2:00 PM',
+    '2:00 PM - 4:00 PM',
+    '4:00 PM - 6:00 PM',
+    '6:00 PM - 8:00 PM',
+  ];
 
   HallModel({this.id, this.capacity});
 
