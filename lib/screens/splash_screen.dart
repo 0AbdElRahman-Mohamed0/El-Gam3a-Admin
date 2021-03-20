65dd4211178858ced264ca75c1593e8198f80a25
kira <sci.bedoMohamed2018@alexu.edu.eg>
2021-03-20 16:06:23 -0400

    logo and app icons

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -6,6 +6,7 @@ import 'package:elgam3a_admin/screens/sign_in_screen.dart';
 import 'package:elgam3a_admin/widgets/error_pop_up.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -88,40 +89,8 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        // height: MediaQuery.of(context).size.height,
-        // width: MediaQuery.of(context).size.width,
-        // color: Theme.of(context).buttonColor,
-        child: Center(
-          child: Hero(
-            tag: 'logo',
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Image.asset(
-                  'assets/icons/ph.png',
-                  color: Theme.of(context).primaryColor,
-                  height: 120.0,
-                  width: 120.0,
-                ),
-                Text(
-                  'El-Gam3a',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline6
-                      .copyWith(fontSize: 64),
-                ),
-                Text(
-                  'Admin',
-                  style: Theme.of(context)
-                      .textTheme
-                      .headline1
-                      .copyWith(fontSize: 20),
-                ),
-              ],
-            ),
-          ),
-        ),
+      body: Center(
+        child: SvgPicture.asset('assets/svg/big logo.svg'),
       ),
     );
   }
